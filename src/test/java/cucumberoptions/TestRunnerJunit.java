c147803f0137cabdf3410113c526e8cfb03eaf33
Juanes <j.moncada@globant.com>
2023-11-07 16:42:10 -0500

    Added Junit paraller execution in POM. Renamed runner file to TestRunnerJunit

--- src/test/java/cucumberoptions/TestRunnerJunit.java
+++ src/test/java/cucumberoptions/TestRunnerJunit.java
@@ -7,6 +7,6 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src/test/java/features", glue = "stepdefinitions", monochrome = true, tags = "@PlaceOrder or @Offers",
         plugin = {"html:target/cucumber.html", "json:target/cucumber.json", "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:", "rerun:target/failed_scenarios.txt"})
-public class JunitTestRunner {
+public class TestRunnerJunit {
 
 }
